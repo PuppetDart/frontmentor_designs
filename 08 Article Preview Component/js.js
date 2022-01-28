3966bcef9e6319cedd2e06e2dfcf280e2c1eb79a
Abhishek Pandey <chiseldart@hotmail.com>
2022-01-28 13:44:07 +0530

    Article Preview Component

--- 08 Article Preview Component/js.js
+++ 08 Article Preview Component/js.js
@@ -2,8 +2,13 @@ var share_icon=document.querySelector(".share_icon")
 var share_menu=document.querySelector(".share_menu")
 var share_arrow=document.querySelector(".share_arrow")
 
+var profile=document.querySelector(".profile")
+var indiv=document.querySelector(".indiv")
+
 share_icon.addEventListener('click', ()=>{
     share_menu.classList.toggle("visible_menu")
     share_icon.classList.toggle("clicked_icon")
     share_arrow.classList.toggle("clicked_arrow")
+    profile.classList.toggle("change_state")
+    indiv.classList.toggle("absent")
 })
