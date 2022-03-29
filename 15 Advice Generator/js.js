366e47c4a92a68e5030b3a88525aab0263ef405a
Abhishek Pandey <chiseldart@hotmail.com>
2022-03-30 02:59:52 +0530

    commented the broken height fixture code

--- 15 Advice Generator/js.js
+++ 15 Advice Generator/js.js
@@ -15,11 +15,11 @@ const rollDice=()=>{
         document.querySelector('.main__quote').textContent='\"'+data.slip.advice+'\"'
         document.querySelector(".title__adviceId").textContent=data.slip.id
     })
-    setTimeout(()=>{
-        mainDiv_totalHeight=document.querySelector(".main__quote").clientHeight+11+56+16+64+48+28
-        document.querySelector(".main").style.height=mainDiv_totalHeight+"px"
-        console.log(mainDiv_totalHeight+"px")
-    }, 200)
+    // setTimeout(()=>{
+    //     mainDiv_totalHeight=document.querySelector(".main__quote").clientHeight+11+56+16+64+48+28
+    //     document.querySelector(".main").style.height=mainDiv_totalHeight+"px"
+    //     console.log(mainDiv_totalHeight+"px")
+    // }, 200)
 }
 
 // let observer = new MutationObserver(mutations=>{
