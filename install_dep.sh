fd322ef2fc49da41f1b83eacafde0e993895a9b9
Abhishek Pandey <chiseldart@hotmail.com>
2022-09-16 11:29:22 +0530

    modified:   install_dep.sh

--- install_dep.sh
+++ install_dep.sh
@@ -3,9 +3,14 @@ ls
 cd /var/www/html
 pwd
 ls
+echo "curl -o"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+echo "nvm/nvm.sh"
 . ~/.nvm/nvm.sh
+echo "nvm install --lts"
 nvm install --lts
+echo "node -v"
 node -v
+echo "npm -v"
 npm -v
 sudo npm install
