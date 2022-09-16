fbe4b7bec3c3718e22fcd62be5ac106653c50e90
Abhishek Pandey <chiseldart@hotmail.com>
2022-09-16 11:41:15 +0530

    new file:   after_install.sh 	modified:   appspec.yml 	modified:   install_dep.sh

--- install_dep.sh
+++ install_dep.sh
@@ -1,16 +1,2 @@
-pwd
-ls
-cd /var/www/html
-pwd
-ls
-echo "curl -o"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-echo "nvm/nvm.sh"
-. ~/.nvm/nvm.sh
-echo "nvm install --lts"
-nvm install --lts
-echo "node -v"
-node -v
-echo "npm -v"
-npm -v
-sudo npm install
+sudo su
+yum update -y
